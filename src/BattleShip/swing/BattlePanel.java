a0bb046916f865680715c66324a8a4df6ae05c1e
suzuki-rikiya <entertainer11127337@gmail.com>
2017-06-30 00:17:30 +0900

    edit BattlePanel

--- src/BattleShip/swing/BattlePanel.java
+++ src/BattleShip/swing/BattlePanel.java
@@ -21,13 +21,17 @@ public class BattlePanel extends JPanel {
         layout.setConstraints(cp, gbc);
         this.add(cp);
 
+        //outp -> sp -> inp の順番になっています
+        JPanel outp = new JPanel();
+        outp.setPreferredSize(new Dimension(870, 290));//spよりも小さくないと画面がつぶれました
+        //outp.setPreferredSize(new Dimension(880, 300));
         JScrollPane sp = new JScrollPane();
         sp.setPreferredSize(new Dimension(880, 300));
-        JPanel pn_others = new JPanel();
+        JPanel inp = new JPanel();
         GridBagLayout scrollLayout = new GridBagLayout();
         GridBagConstraints gbc_maps = new GridBagConstraints();
-        AttackCellPanel[] pn_maps = new AttackCellPanel[6];
-        pn_others.setLayout(scrollLayout);
+        AttackCellPanel[] pn_maps = new AttackCellPanel[6];//groupに属するAttackerの数を取得
+        inp.setLayout(scrollLayout);
         for (int i = 0; i < 6; i++) {
             pn_maps[i] = new AttackCellPanel(mf, this, 25);
             gbc_maps.gridx = i % 3;
@@ -36,16 +40,16 @@ public class BattlePanel extends JPanel {
             gbc_maps.weighty = 1.0d;
             gbc_maps.insets = new Insets(10, 10, 10, 10);
             scrollLayout.setConstraints(pn_maps[i], gbc_maps);
-            pn_others.add(pn_maps[i]);
+            inp.add(pn_maps[i]);//それぞれのCellPanelはinpに設置
         }
-        sp.setViewportView(pn_others);
-        sp.setBounds(0, 450, 880, 300);
+        sp.setViewportView(inp);//ScrollPaneの中にinpを設置
         gbc.gridx = 0;
         gbc.gridy = 1;
         gbc.gridwidth = 3;
         gbc.weightx = 1.0d;
         gbc.weighty = 1.0d;
-        layout.setConstraints(sp, gbc);
-        this.add(sp);
+        outp.add(sp);//outpにScrollPaneを配置
+        layout.setConstraints(outp, gbc);//GridBagLayoutはoutpに適用
+        this.add(outp);
     }
 }
